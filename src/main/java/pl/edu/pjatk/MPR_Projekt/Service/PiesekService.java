45a5433a4037e23cbfc9fda4d4a22c9de855694d
domin <dominikdorawa12@gmail.com>
2024-11-04 13:02:48 +0100

    testy zrobione

--- src/main/java/pl/edu/pjatk/MPR_Projekt/Service/PiesekService.java
+++ src/main/java/pl/edu/pjatk/MPR_Projekt/Service/PiesekService.java
@@ -2,6 +2,7 @@ package pl.edu.pjatk.MPR_Projekt.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import pl.edu.pjatk.MPR_Projekt.Model.Piesek;
 import pl.edu.pjatk.MPR_Projekt.Repository.PiesekRepository;
 
@@ -11,21 +12,23 @@ import java.util.Optional;
 
 @Component
 public class PiesekService {
-private PiesekRepository piesekRepository;
-
+    private PiesekRepository piesekRepository;
+    private StringUtilsService stringUtilsService;
     List<Piesek> piesekList = new ArrayList<>();
 
-@Autowired
-    public PiesekService(PiesekRepository repository) {
-    this.piesekRepository = repository;
+    @Autowired
+    public PiesekService(PiesekRepository repository, StringUtilsService stringUtilsService) {
+        this.stringUtilsService = stringUtilsService;
 
-    piesekRepository.save(new Piesek("brown", "Spike", 1));
-    piesekRepository.save(new Piesek("red", "Doggy", 2));
-    piesekRepository.save(new Piesek("orange", "Leo", 3));
+        this.piesekRepository = repository;
+
+//    piesekRepository.save(new Piesek("brown", "Spike", 1));
+//    piesekRepository.save(new Piesek("red", "Doggy", 2));
+//    piesekRepository.save(new Piesek("orange", "Leo", 3));
     }
 
     public List<Piesek> getPiesekByName (String name) {
-    return piesekRepository.findByName(name);
+        return piesekRepository.findByName(name);
     }
 
 
@@ -34,11 +37,13 @@ private PiesekRepository piesekRepository;
     }
 
     public void createPiesek(Piesek piesek) {
-    piesekRepository.save(piesek);
+        piesek.setName(this.stringUtilsService.lower(piesek.getName()));
+        piesek.setName(this.stringUtilsService.upper(piesek.getName()));
+        piesekRepository.save(piesek);
     }
 
     public void deletePiesekById(int id) {
-piesekRepository.deleteById(id);
+        piesekRepository.deleteById(id);
     }
 
 //    Optional<Piesek> piesek = piesekRepository.findById(id);
@@ -49,19 +54,25 @@ piesekRepository.deleteById(id);
 //    }
 
     public void removePiesekByName(String name) {
-    List <Piesek> pieski = piesekRepository.findByName(name);
-    if (pieski.isEmpty()) {
-        throw new IllegalArgumentException("Piesek o nazwie" + name + " nie istnieje");
-    }
+        List <Piesek> pieski = piesekRepository.findByName(name);
+        if (pieski.isEmpty()) {
+            throw new IllegalArgumentException("Piesek o nazwie" + name + " nie istnieje");
+        }
         for (Piesek piesek : pieski) {
             piesekRepository.delete(piesek);
         }
     }
 
 
-public Optional<Piesek> getById(int id) {
-    return this.piesekRepository.findById(id);
-}
+    public Optional<Piesek> getById(int id) {
+
+        Optional<Piesek> piesek =  this.piesekRepository.findById(id);
+
+        if(piesek.isPresent()) {
+            piesek.get().setName(this.stringUtilsService.lower(piesek.get().getName()));
+        }
+        return piesek;
+    }
 
 //    public void updatePiesek(int id, piesek ) {
 //       Optional<Piesek> existingPiesek = piesekRepository.findById(piesek.getId());
@@ -73,17 +84,21 @@ public Optional<Piesek> getById(int id) {
 //    }
 
 
-public void updatePiesek(int id, Piesek updatedPiesek) {
-    Optional<Piesek> existingPiesek = piesekRepository.findById(id);
-    if (existingPiesek.isPresent()) {
-        Piesek piesek = existingPiesek.get();
-        piesek.setName(updatedPiesek.getName());
-        piesek.setColor(updatedPiesek.getColor());
-        piesekRepository.save(piesek);
-    } else {
-        throw new RuntimeException("Piesek o ID " + id + " nie istnieje");
+    public void updatePiesek(int id, Piesek updatedPiesek) {
+        Optional<Piesek> existingPiesek = piesekRepository.findById(id);
+        if (existingPiesek.isPresent()) {
+            Piesek piesek = existingPiesek.get();
+            piesek.setName(updatedPiesek.getName());
+            piesek.setColor(updatedPiesek.getColor());
+            piesekRepository.save(piesek);
+        } else {
+            throw new RuntimeException("Piesek o ID " + id + " nie istnieje");
+        }
     }
-}
+
+
+
+
 
 }
 
